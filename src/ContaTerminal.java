668306c3c8bfa04890de7904930303e0394d481b
Gabriel Martins <gabrielmonte.aluno.institute@gmail.com>
2024-07-15 21:41:30 -0300

    Initial Commit + creating scanner object

---
+++ src/ContaTerminal.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println();
+        String nome = scanner.next();
+
+        System.out.println();
+
+    }
+}
