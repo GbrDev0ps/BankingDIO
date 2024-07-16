ecd40aa78e638d6334cda07aa24714ee9f408eed
Gabriel Martins <gabrielmonte.aluno.institute@gmail.com>
2024-07-15 21:57:19 -0300

    Finishing the code

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -5,10 +5,11 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println();
+        System.out.println("Olá! Aqui é o Banking da DIO");
+
+        System.out.println("Digite seu nome para iniciarmos a abertura de sua conta: ");
         String nome = scanner.next();
 
-        System.out.println();
-
+        System.out.println("Olá " + nome + ", sua conta foi criada com sucesso aqui no Banking DIO!");
     }
 }
